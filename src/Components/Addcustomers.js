1f40dac1ab2d9af24e7fe17add874c7cc2d2278b
rajag <rajagurru333@gmail.com>
2024-01-27 10:22:46 +0530

    chan

--- src/Components/Addcustomers.js
+++ src/Components/Addcustomers.js
@@ -37,7 +37,7 @@ const Addcustomers = ({ userData, setUserData }) => {
             Email,
             Age,
             Gender,
-            Address,
+            Address:Address,
             City,
             State,
             Pincode,

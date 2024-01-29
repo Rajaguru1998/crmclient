ec84a61a6447921ea5f8925b01368f0085687539
rajag <rajagurru333@gmail.com>
2024-01-29 18:40:02 +0530

    changess

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -13,8 +13,7 @@ const LoginPage = () => {
             email,
             password
         }
-        // const response = await fetch(`https://crm-a60b.onrender.com/api/user/login`, {
-            // const response =await fetch(`https://crmserver-7k9y.onrender.com/api/user/login`,{  
+                   // const response =await fetch(`https://crmserver-7k9y.onrender.com/api/user/login`,{  
                 const response =await fetch(`https://crm-server-l94c.onrender.com/api/user/login`,{  
             method: "POST",
             body: JSON.stringify(userDetails),
